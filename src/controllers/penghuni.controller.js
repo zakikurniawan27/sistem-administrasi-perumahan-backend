3c20f76feab2c123d602382c8650da803051a1e0
zakikurniawan27 <kurniawan.zak279@gmail.com>
2024-07-18 21:18:26 +0800

    feature/penghuni: add feature update data penghuni

--- src/controllers/penghuni.controller.js
+++ src/controllers/penghuni.controller.js
@@ -1,10 +1,9 @@
 const { penghuni } = require("../models");
 
 const addPenghuni = async (req, res) => {
-  const { nama_lengkap, status_hunian, nomor_telepon, status_pernikahan } =
-    req.body;
-
   try {
+    const { nama_lengkap, status_hunian, nomor_telepon, status_pernikahan } =
+      req.body;
     if (
       !nama_lengkap ||
       !status_hunian ||
@@ -54,4 +53,58 @@ const getDataPenghuni = async (req, res) => {
   }
 };
 
-module.exports = { addPenghuni, getDataPenghuni };
+const getDetailDataPenghuni = async (req, res) => {
+  try {
+    const idPenghuni = req.params.id;
+    const data = await penghuni.findByPk(idPenghuni);
+
+    return res.status(200).send({
+      status: 200,
+      message: "get detail data penghuni success",
+      detail: data,
+    });
+  } catch (error) {
+    return res.status(404).send({
+      status: 404,
+      message: "get detail data penghuni failed",
+    });
+  }
+};
+
+const updateDataPenghuni = async (req, res) => {
+  try {
+    const idPenghuni = req.params.id;
+    const { nama_lengkap, status_hunian, nomor_telepon, status_pernikahan } =
+      req.body;
+
+    await penghuni.update(
+      {
+        nama_lengkap,
+        foto_ktp: req.file.path,
+        status_hunian,
+        nomor_telepon,
+        status_pernikahan,
+      },
+      {
+        where: { id: idPenghuni },
+      }
+    );
+
+    return res.status(201).send({
+      status: 201,
+      message: "update data penghuni success",
+    });
+  } catch (error) {
+    return res.status(404).send({
+      status: 404,
+      message: "update data penghuni failed",
+    });
+  }
+};
+
+module.exports = {
+  addPenghuni,
+  getDataPenghuni,
+  getDetailDataPenghuni,
+  updateDataPenghuni,
+};
--- src/routes/penghuni.route.js
+++ src/routes/penghuni.route.js
@@ -2,12 +2,20 @@ const express = require("express");
 const {
   addPenghuni,
   getDataPenghuni,
+  getDetailDataPenghuni,
+  updateDataPenghuni,
 } = require("../controllers/penghuni.controller");
 const { upload } = require("../middlewares/upload");
 
 const router = express.Router();
 
 router.get("/", getDataPenghuni);
+router.get("/detail/penghuni/:id", getDetailDataPenghuni);
 router.post("/tambahPenghuni", upload.single("foto_ktp"), addPenghuni);
+router.put(
+  "/update/penghuni/:id",
+  upload.single("foto_ktp"),
+  updateDataPenghuni
+);
 
 module.exports = router;
